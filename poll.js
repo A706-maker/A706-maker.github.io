d2461c415cc4e4c503acfff21e31be9735453d77
A706-maker <akriti.biswal@education.nsw.gov.au>
2026-03-26 20:39:40 +1100

    Update poll.js

--- poll.js
+++ poll.js
@@ -2,22 +2,22 @@ const questions = [
 {
   text: "What is the #1 cause of Sydney's housing affordability crisis?",
   options: ["Foreign investment driving up prices", "Population growth outpacing infastructure", "Insufficient housing supply", "Low interest rates inflated over the past decade"],
-  correct: 3
+  correct: 2
 },
 {
   text: "What salary do you think would be required to buy a home today?",
   options: ["130k", "172k", "224k", "232k"],
-  correct: 4
+  correct: 3
 },
 {
   text: "What does negative gearing allow property investors to do?",
   options: ["Claim rental losses as a tax deduction", "Guarantee profit", "Generate cash flow", "Protect investors against rising interest rates"],
-  correct: 1
+  correct: 0
 },
 {
   text: "The 'First Home Guarantee' scheme in NSW now allows eligible buyers to purcahse properties valued up to _____ with only a 5% deposit",
   options: ["$900k", "$1.5M", "$1.75M", "$1.8M"],
-  correct: 2
+  correct: 1
 }
 ];
 
@@ -40,7 +40,7 @@ questions.forEach((q, index) => {
       if (i ===q.correct) {
         alert("Correct!");
       } else {
-        alert("Incorrect.);
+        alert("Incorrect.");
               }
     };
 
